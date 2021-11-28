116431ddae20e7ba354a310df6807d39ed462555
ultram4rine <ultramarine730@gmail.com>
2021-11-28 15:27:18 +0400

    fix

--- non-archimedean-analysis/corput/src/main.rs
+++ non-archimedean-analysis/corput/src/main.rs
@@ -1,15 +1,19 @@
+#![allow(non_snake_case)]
+
 use plotters::prelude::*;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let n = 25 as i64;
+    let n = 100 as i64;
     let b = 2 as i64;
 
+    let name = format!("graphs_n_{}.png", n);
+
     // create graph picture and fill it by white color.
-    let root = BitMapBackend::new("graph.png", (2000, 1000)).into_drawing_area();
+    let root = BitMapBackend::new(&name, (2000, 1000)).into_drawing_area();
     root.fill(&WHITE)?;
     let (left, right) = root.split_horizontally(1000);
 
-    let make_chart = |area, from_x, to_x, from_y, to_y| {
+    let make_chart = |area, (from_x, to_x), (from_y, to_y)| {
         ChartBuilder::on(area)
             .caption("", ("sans-serif", 50).into_font())
             .margin(5)
@@ -17,37 +21,29 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             .y_label_area_size(30)
             .build_cartesian_2d(from_x..to_x, from_y..to_y)
     };
-    let mut chart_left = make_chart(&left, -0.1, 1.1, -0.1, 1.1)?;
+    let mut chart_left = make_chart(&left, (-0.1, 1.1), (-0.1, 1.1))?;
     chart_left.configure_mesh().draw()?;
-    let mut chart_right = make_chart(&right, -0.1_f64, n as f64 + 0.1, -0.1, 1.1)?;
+    let mut chart_right = make_chart(&right, (-0.1_f64, (n - 1) as f64 + 0.1), (-0.1, 1.1))?;
     chart_right.configure_mesh().draw()?;
 
-    for k in 0..=n {
-        chart_left.draw_series(PointSeries::of_element(
-            ((1_f64 - 1_f64 / b.pow(k as u32) as f64)
-                ..(1_f64 - 1_f64 / b.pow((k + 1) as u32) as f64))
-                .step(1_f64 / (10 * n) as f64)
-                .values()
-                .map(|x| (x, T(x, b, k as u32))),
-            1,
-            &RED,
-            &|c, s, st| EmptyElement::at(c) + Circle::new((0, 0), s, st.filled()),
-        ))?;
-    }
-
+    let vdc_seq_R = vdc_R(b, n);
     chart_left
         .draw_series(PointSeries::of_element(
-            (0..0).map(|x| (x as f64, x as f64)),
-            1,
+            (0..n).map(|i| (vdc_seq_R[i as usize], vdc_seq_R[i as usize + 1])),
+            2,
             &RED,
-            &|c, _s, _st| EmptyElement::at(c),
+            &|c, s, st| EmptyElement::at(c) + Circle::new((0, 0), s, st.filled()),
         ))?
         .label("Kakutani - von Neumann")
         .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
 
+    let vdc_seq_Z2 = vdc_Z2(n);
     chart_right
         .draw_series(LineSeries::new(
-            (0_i64..=n).map(|x| (x as f64, phi(x))),
+            (0..n).map(|i| {
+                let x = vdc_seq_Z2[i as usize];
+                (x as f64, phi(x))
+            }),
             &BLUE,
         ))?
         .label("Monna")
@@ -67,10 +63,42 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-#[allow(non_snake_case)]
+/// Build the van der Corput sequence from Kakutani - von Neumann transformation.
+fn vdc_R(b: i64, n: i64) -> Vec<f64> {
+    let mut x = 0_f64;
+    let mut seq = vec![];
+    for _ in 0..=n {
+        seq.push(x);
+
+        let mut k = 0;
+        while x < 1_f64 - (1_f64 / b.pow(k) as f64) || x >= 1_f64 - (1_f64 / b.pow(k + 1) as f64) {
+            k += 1;
+        }
+
+        x = T(x, b, k);
+    }
+    seq
+}
+
+/// Build the van der Corput sequence from Kakutani - von Neumann transformation.
+fn vdc_Z2(n: i64) -> Vec<i64> {
+    let mut x = 0_i64;
+    let mut seq = vec![];
+    for _ in 0..=n {
+        seq.push(x);
+        x = tau(x);
+    }
+    seq
+}
+
 /// Kakutani - von Neumann
 fn T(x: f64, b: i64, k: u32) -> f64 {
     x - 1_f64 + (1_f64 / b.pow(k) as f64) + (1_f64 / b.pow(k + 1) as f64)
+}
+
+/// Z2 analogue of Kakutani - von Neumann mapping
+fn tau(x: i64) -> i64 {
+    x + 1
 }
 
 /// Monna
