39b8cc3b8e844a85fc06d681d2819a63536fc9db
ultram4rine <ultramarine730@gmail.com>
2023-03-29 23:31:22 +0400

    spring init

---
+++ parallel/eurovision-parallel/src/main/java/com/github/ultram4rine/ssu/parallel/eurovisionparallel/EurovisionParallelApplication.java
@@ -0,0 +1,13 @@
+package com.github.ultram4rine.ssu.parallel.eurovisionparallel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurovisionParallelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurovisionParallelApplication.class, args);
+	}
+
+}
---
+++ parallel/eurovision-parallel/src/test/java/com/github/ultram4rine/ssu/parallel/eurovisionparallel/EurovisionParallelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ultram4rine.ssu.parallel.eurovisionparallel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurovisionParallelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
