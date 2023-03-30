aadca74ad38d93e1c711cacbb44ccd36acc67448
ultram4rine <ultramarine730@gmail.com>
2023-03-31 00:30:04 +0400

    fix etl, db, update dockerfile

--- parallel/eurovision-parallel/etl.py
+++ parallel/eurovision-parallel/etl.py
@@ -37,7 +37,7 @@ with zipfile.ZipFile("archive.zip") as zf:
             db_data = []
             for row in dict:
                 year = row["year"]
-                semi_draw_position = row["semi_draw_position"]
+                semi_draw_position = None
                 final_draw_position = None
                 country = row["country"]
                 artist_name = row["artist_name"]
@@ -46,25 +46,25 @@ with zipfile.ZipFile("archive.zip") as zf:
                 style = row["style"]
                 direct_qualifier_10 = None
                 gender = row["gender"]
-                main_singers = row["main_singers"]
+                main_singers = None
                 age = row["age"]
                 selection = row["selection"]
                 key = row["key"]
-                BPM = row["BPM"]
-                energy = row["energy"]
-                danceability = row["danceability"]
-                happiness = row["happiness"]
+                BPM = None
+                energy = None
+                danceability = None
+                happiness = None
                 loudness = row["loudness"]
-                acousticness = row["acousticness"]
-                instrumentalness = row["instrumentalness"]
-                liveness = row["liveness"]
+                acousticness = None
+                instrumentalness = None
+                liveness = None
                 speechiness = row["speechiness"]
                 release_date = row["release_date"]
                 key_change_10 = row["key_change_10"]
-                backing_dancers = row["backing_dancers"]
-                backing_singers = row["backing_singers"]
-                backing_instruments = row["backing_instruments"]
-                instrument_10 = row["instrument_10"]
+                backing_dancers = None
+                backing_singers = None
+                backing_instruments = None
+                instrument_10 = None
                 qualified = None
                 final_televote_points = None
                 final_jury_points = None
@@ -74,31 +74,63 @@ with zipfile.ZipFile("archive.zip") as zf:
                 final_total_points = None
                 semi_place = None
                 semi_total_points = None
-                favourite_10 = row["favourite_10"]
+                favourite_10 = None
                 race = row["race"]
-                host_10 = row["host_10"]
-                if row["final_draw_position"] != "-":
+                host_10 = None
+
+                if row["semi_draw_position"] != "-" and row["semi_draw_position"] != "":
+                    semi_draw_position = row["semi_draw_position"]
+                if row["final_draw_position"] != "-" and row["final_draw_position"] != "":
                     final_draw_position = row["final_draw_position"]
-                if row["direct_qualifier_10"] != "-":
+                if row["direct_qualifier_10"] != "-" and row["direct_qualifier_10"] != "":
                     direct_qualifier_10 = row["direct_qualifier_10"]
-                if row["qualified"] != "-":
+                if row["main_singers"] != "-" and row["main_singers"] != "":
+                    main_singers = row["main_singers"]
+                if row["qualified"] != "-" and row["qualified"] != "":
                     qualified = row["qualified"]
-                if row["final_televote_points"] != "-":
+                if row["BPM"] != "-" and row["BPM"] != "":
+                    BPM = row["BPM"]
+                if row["energy"] != "-" and row["energy"] != "":
+                    energy = row["energy"]
+                if row["danceability"] != "-" and row["danceability"] != "":
+                    danceability = row["danceability"]
+                if row["happiness"] != "-" and row["happiness"] != "":
+                    happiness = row["happiness"]
+                if row["acousticness"] != "-" and row["acousticness"] != "":
+                    acousticness = row["acousticness"]
+                if row["instrumentalness"] != "-" and row["instrumentalness"] != "":
+                    instrumentalness = row["instrumentalness"]
+                if row["liveness"] != "-" and row["liveness"] != "":
+                    liveness = row["liveness"]
+                if row["backing_dancers"] != "-" and row["backing_dancers"] != "":
+                    backing_dancers = row["backing_dancers"]
+                if row["backing_singers"] != "-" and row["backing_singers"] != "":
+                    backing_singers = row["backing_singers"]
+                if row["backing_instruments"] != "-" and row["backing_instruments"] != "":
+                    backing_instruments = row["backing_instruments"]
+                if row["instrument_10"] != "-" and row["instrument_10"] != "":
+                    instrument_10 = row["instrument_10"]
+                if row["final_televote_points"] != "-" and row["final_televote_points"] != "":
                     final_televote_points = row["final_televote_points"]
-                if row["final_jury_points"] != "-":
+                if row["final_jury_points"] != "-" and row["final_jury_points"] != "":
                     final_jury_points = row["final_jury_points"]
-                if row["final_televote_votes"] != "-":
+                if row["final_televote_votes"] != "-" and row["final_televote_votes"] != "":
                     final_televote_votes = row["final_televote_votes"]
-                if row["final_jury_votes"] != "-":
+                if row["final_jury_votes"] != "-" and row["final_jury_votes"] != "":
                     final_jury_votes = row["final_jury_votes"]
-                if row["final_place"] != "-":
+                if row["final_place"] != "-" and row["final_place"] != "":
                     final_place = row["final_place"]
-                if row["final_total_points"] != "-":
+                if row["final_total_points"] != "-" and row["final_total_points"] != "":
                     final_total_points = row["final_total_points"]
-                if row["semi_place"] != "-":
+                if row["semi_place"] != "-" and row["semi_place"] != "":
                     semi_place = row["semi_place"]
-                if row["semi_total_points"] != "-":
+                if row["semi_total_points"] != "-" and row["semi_total_points"] != "":
                     semi_total_points = row["semi_total_points"]
+                if row["favourite_10"] != "-" and row["favourite_10"] != "":
+                    favourite_10 = row["favourite_10"]
+                if row["host_10"] != "-" and row["host_10"] != "":
+                    host_10 = row["host_10"]
+
                 db_data.append((year, semi_draw_position, final_draw_position, country, artist_name, song_name, language, style, direct_qualifier_10, gender, main_singers, age, selection, key, BPM, energy, danceability, happiness, loudness, acousticness, instrumentalness, liveness, speechiness,
                                release_date, key_change_10, backing_dancers, backing_singers, backing_instruments, instrument_10, qualified, final_televote_points, final_jury_points, final_televote_votes, final_jury_votes, final_place, final_total_points, semi_place, semi_total_points, favourite_10, race, host_10))
             cur.executemany(
@@ -111,7 +143,7 @@ with zipfile.ZipFile("archive.zip") as zf:
             con.commit()
         elif "contest_data" in filename:
             db_data = [(row["year"], row["host"], row["date"], row["semi_countries"], row["final_countries"],
-                        row["jury_countries_voting_final"], row["televote_countries_voting_final"] or None) for row in dict]
+                        row["jury_countries_voting_final"] or 0, row["televote_countries_voting_final"] or 0) for row in dict]
             cur.executemany(
                 """INSERT INTO contests 
                 (year, host, date, semi_countries, final_countries, 
--- parallel/eurovision-parallel/schema.sql
+++ parallel/eurovision-parallel/schema.sql
@@ -9,35 +9,35 @@ CREATE TABLE IF NOT EXISTS countries (
     region TEXT NOT NULL
 );
 CREATE TABLE IF NOT EXISTS songs (
-    year INTEGER NOT NULL,
+    year TEXT NOT NULL,
     country TEXT NOT NULL,
     artist_name TEXT NOT NULL,
     song_name TEXT NOT NULL,
-    semi_draw_position INTEGER NOT NULL,
+    semi_draw_position INTEGER NULL,
     final_draw_position INTEGER NULL,
     language TEXT NOT NULL,
     style TEXT NOT NULL,
     direct_qualifier_10 INTEGER NULL,
     gender TEXT NOT NULL,
-    main_singers INTEGER NOT NULL,
+    main_singers INTEGER NULL,
     age TEXT NOT NULL,
     selection TEXT NOT NULL,
     key TEXT NOT NULL,
-    bpm INTEGER NOT NULL,
-    energy INTEGER NOT NULL,
-    danceability INTEGER NOT NULL,
-    happiness INTEGER NOT NULL,
+    bpm INTEGER NULL,
+    energy INTEGER NULL,
+    danceability INTEGER NULL,
+    happiness INTEGER NULL,
     loudness TEXT NOT NULL,
-    acousticness INTEGER NOT NULL,
-    instrumentalness INTEGER NOT NULL,
-    liveness INTEGER NOT NULL,
-    speechiness INTEGER NOT NULL,
+    acousticness INTEGER NULL,
+    instrumentalness INTEGER NULL,
+    liveness INTEGER NULL,
+    speechiness TEXT NOT NULL,
     release_date TEXT NOT NULL,
     key_change_10 TEXT NOT NULL,
-    backing_dancers INTEGER NOT NULL,
-    backing_singers INTEGER NOT NULL,
-    backing_instruments INTEGER NOT NULL,
-    instrument_10 INTEGER NOT NULL,
+    backing_dancers INTEGER NULL,
+    backing_singers INTEGER NULL,
+    backing_instruments INTEGER NULL,
+    instrument_10 INTEGER NULL,
     qualified INTEGER NULL,
     final_televote_points INTEGER NULL,
     final_jury_points INTEGER NULL,
@@ -47,38 +47,38 @@ CREATE TABLE IF NOT EXISTS songs (
     final_total_points INTEGER NULL,
     semi_place INTEGER NULL,
     semi_total_points INTEGER NULL,
-    favourite_10 INTEGER NOT NULL,
+    favourite_10 INTEGER NULL,
     race TEXT NOT NULL,
-    host_10 INTEGER NOT NULL
+    host_10 INTEGER NULL
 );
 CREATE TABLE IF NOT EXISTS contests (
-    year INTEGER NOT NULL,
+    year TEXT NOT NULL,
     host TEXT NOT NULL,
     date TEXT NOT NULL,
     semi_countries INTEGER NOT NULL,
     final_countries INTEGER NOT NULL,
-    jury_countries_voting_final INTEGER NOT NULL,
+    jury_countries_voting_final INTEGER NULL,
     televote_countries_voting_final INTEGER NULL
 );
 CREATE TABLE IF NOT EXISTS jury_results (
-    year INTEGER NOT NULL,
+    year TEXT NOT NULL,
     contestant TEXT NOT NULL,
     country TEXT NOT NULL,
     score INTEGER NOT NULL
 );
 CREATE TABLE IF NOT EXISTS televote_results (
-    year INTEGER NOT NULL,
+    year TEXT NOT NULL,
     contestant TEXT NOT NULL,
     country TEXT NOT NULL,
     score INTEGER NOT NULL
 );
 CREATE TABLE IF NOT EXISTS eurovision_world_poll (
-    year INTEGER NOT NULL,
+    year TEXT NOT NULL,
     contestant TEXT NOT NULL,
     votes INTEGER NOT NULL
 );
 CREATE TABLE IF NOT EXISTS ogae_poll (
-    year INTEGER NOT NULL,
+    year TEXT NOT NULL,
     contestant TEXT NOT NULL,
     average_points REAL NOT NULL
 );
